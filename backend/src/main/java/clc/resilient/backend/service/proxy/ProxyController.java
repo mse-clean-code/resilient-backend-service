aa2201c4908643347b0ed66d795fcc0304cfc6a1
Kacper Urbaniec <at.kacper.urbaniec@gmail.com>
2023-12-23 11:35:53 +0100

    Added proxy endpoint for `tmdb` images

--- backend/src/main/java/clc/resilient/backend/service/proxy/ProxyController.java
+++ backend/src/main/java/clc/resilient/backend/service/proxy/ProxyController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +17,12 @@ import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Enumeration;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 /**
@@ -56,6 +60,7 @@ public class ProxyController {
             .query(request.getQueryString())
             .build(true).toUri();
 
+        // Set all headers besides encoding (explained later)
         HttpHeaders headers = new HttpHeaders();
         Enumeration<String> headerNames = request.getHeaderNames();
         while (headerNames.hasMoreElements()) {
@@ -100,5 +105,81 @@ public class ProxyController {
         logger.debug("Custom List Action | {} | {}", method.name(), request.getRequestURI());
 
         return mirrorRest(body, method, request, response);
+    }
+
+    @GetMapping(value = "/image.tmdb/**", produces = "application/octet-stream")
+    public void image(@RequestBody(required = false) String body,
+                                              HttpMethod method, HttpServletRequest request, HttpServletResponse response)
+        throws URISyntaxException, IOException {
+
+        logger.debug("Image | {} | {}", method.name(), request.getRequestURI());
+
+        // TODO: Create tmdb client
+
+        // Based on https://stackoverflow.com/a/49429650/12347616
+        String requestUrl = request
+            .getRequestURI()
+            .replaceAll("^/image.tmdb", "");
+
+        URI uri = new URI("https", null, "image.tmdb.org", 443, null, null, null);
+        uri = UriComponentsBuilder.fromUri(uri)
+            .path(requestUrl)
+            .query(request.getQueryString())
+            .build(true).toUri();
+
+        // Set all headers besides encoding (explained later)
+        HttpHeaders headers = new HttpHeaders();
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            if (Objects.equals(headerName, "Accept-Encoding")) continue;
+            headers.set(headerName, request.getHeader(headerName));
+        }
+
+        // Fix encoding
+        // Lead to 'Undocumented Error:OK' on Swagger
+        // Debugger showed (in Chromium only) net::ERR_INCOMPLETE_CHUNKED_ENCODING 200 (OK)
+        // Fix: https://stackoverflow.com/questions/54852953/postman-shows-could-not-get-any-response-even-though-response-is-ok
+        headers.set("Accept-Encoding", "*/*");
+
+        // API key
+        // TODO: Move to config
+        headers.set("Authorization", "Bearer " + "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzY2ZjNmUxZGQyMzFiZDFmMmNhYTE5OGU3MzE3YTZhNCIsInN1YiI6IjYwZWZiOTZlYTQ0ZDA5MDAyZDQ0ZjNlNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nvvleDHS5FWTK9UbhKfeuW8L5w4hyjGHAphNtQJuYSY");
+
+        HttpEntity<String> httpEntity = new HttpEntity<>(body, headers);
+
+        // TODO Add restTemplate bean in order to mock
+        RestTemplate restTemplate = new RestTemplate();
+        try {
+            // Based on https://stackoverflow.com/a/62528659/12347616
+            restTemplate.execute(uri, method, null, clientHttpResponse -> {
+                StreamUtils.copy(clientHttpResponse.getBody(), response.getOutputStream());
+                return null;
+            });
+        } catch(HttpStatusCodeException e) {
+            populateResponse(
+                ResponseEntity.status(e.getStatusCode())
+                     .headers(e.getResponseHeaders())
+                     .body(e.getResponseBodyAsString()),
+                response
+            );
+        }
+    }
+
+    // https://stackoverflow.com/a/51655139/12347616
+    public static void populateResponse(ResponseEntity<String> responseEntity, HttpServletResponse servletResponse)
+        throws IOException {
+        for (Map.Entry<String, List<String>> header : responseEntity.getHeaders().entrySet()) {
+            String chave = header.getKey();
+            for (String valor : header.getValue()) {
+                servletResponse.addHeader(chave, valor);
+            }
+        }
+
+        servletResponse.setStatus(responseEntity.getStatusCode().value());
+        if (responseEntity.hasBody()) {
+            servletResponse.getWriter().write(responseEntity.getBody());
+        }
+
     }
 }
