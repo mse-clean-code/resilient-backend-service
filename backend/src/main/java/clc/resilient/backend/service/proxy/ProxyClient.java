02ca6b28b092d1cf5c06710e3b15582467b1099d
Kacper Urbaniec <at.kacper.urbaniec@gmail.com>
2023-12-23 15:13:35 +0100

    `ProxyClient` now also streamlines `tmdb` image api access

--- backend/src/main/java/clc/resilient/backend/service/proxy/ProxyClient.java
+++ backend/src/main/java/clc/resilient/backend/service/proxy/ProxyClient.java
@@ -1,6 +1,7 @@
 package clc.resilient.backend.service.proxy;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.constraints.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +11,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -24,27 +26,33 @@ import java.util.Objects;
  */
 @Component
 public class ProxyClient {
-    private final URI baseUri;
+    private final URI apiUri;
     private final String apiReadAccessKey;
+    private final URI imageUri;
     private final RestTemplate restTemplate = new RestTemplate();
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     ProxyClient(
-        @Value("${tmdb.api.uri}") String uri,
-        @Value("${tmdb.api.read-access-key}") String apiReadAccessKey
+        @Value("${tmdb.api.uri}") String apiUri,
+        @Value("${tmdb.api.read-access-key}") String apiReadAccessKey,
+        @Value("${tmdb.image.uri}") String imageUri
     ) throws URISyntaxException {
-        this.baseUri = new URI(uri);
+        this.apiUri = new URI(apiUri);
         this.apiReadAccessKey = apiReadAccessKey;
+        this.imageUri = new URI(imageUri);
     }
 
-    // https://developer.themoviedb.org/reference/intro/getting-started
+    /**
+     * Method for fetching resources from tmdb api.
+     * See <a href="https://developer.themoviedb.org/reference/intro/getting-started">docs</a>.
+     */
     public ResponseEntity<String> fetchTmdbApi(
         @NotNull HttpMethod method, @NotNull String path,
         @NotNull HttpServletRequest request, String body
     ) {
         // Proxy fetch based on https://stackoverflow.com/a/49429650/12347616
         logger.debug("fetchTmdbApi({}, {}, {}, {})", method, path, request.getQueryString(), body);
-        var uri = buildPathURI(path, request);
+        var uri = buildPathURI(apiUri, path, request);
         var headers = buildHeaders(request);
         var httpEntity = new HttpEntity<>(body, headers);
 
@@ -58,8 +66,34 @@ public class ProxyClient {
         }
     }
 
-    private URI buildPathURI(String path, HttpServletRequest request) {
-        return UriComponentsBuilder.fromUri(baseUri)
+    /**
+     * Method for fetching image resources from tmdb image api.
+     * See <a href="https://developer.themoviedb.org/docs/image-basics">docs</a>.
+     */
+    public void fetchTmdbImage(
+        @NotNull HttpMethod method, @NotNull String path,
+        @NotNull HttpServletRequest request, String body,
+        @NotNull HttpServletResponse response
+        ) {
+        // File fetch based on https://stackoverflow.com/a/62528659/12347616
+        logger.debug("fetchTmdbImage({}, {}, {}, {}, {})", method, path, request.getQueryString(), body, response);
+        var uri = buildPathURI(imageUri, path, request);
+
+        try {
+            restTemplate.execute(uri, method, null, clientHttpResponse -> {
+                StreamUtils.copy(clientHttpResponse.getBody(), response.getOutputStream());
+                return null;
+            });
+        } catch (ResourceAccessException ex) {
+            // Throw custom exception when external API is offline
+            // https://stackoverflow.com/a/52271541/12347616
+            logger.error("tmdb is offline");
+            throw new TmdbOfflineException(ex);
+        }
+    }
+
+    private URI buildPathURI(URI base, String path, HttpServletRequest request) {
+        return UriComponentsBuilder.fromUri(base)
             .path(path)
             .query(request.getQueryString())
             .build(true).toUri();
@@ -79,13 +113,8 @@ public class ProxyClient {
         // Debugger showed (in Chromium only) net::ERR_INCOMPLETE_CHUNKED_ENCODING 200 (OK)
         // Fix: https://stackoverflow.com/questions/54852953/postman-shows-could-not-get-any-response-even-though-response-is-ok
         headers.set("Accept-Encoding", "*/*");
-
         // Set API key
         headers.set("Authorization", "Bearer " + apiReadAccessKey);
         return headers;
     }
-
-
-    // https://developer.themoviedb.org/docs/image-basics
-
 }
--- backend/src/main/java/clc/resilient/backend/service/proxy/ProxyController.java
+++ backend/src/main/java/clc/resilient/backend/service/proxy/ProxyController.java
@@ -5,26 +5,12 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.HttpStatusCodeException;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.Enumeration;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 
 /**
  * @author Kacper Urbaniec
@@ -45,21 +31,39 @@ public class ProxyController {
         return "test";
     }
 
-    // Catches all tmdb operations besides lists
+    /**
+     * Endpoint that proxies all tmdb methods besides list functionality.
+     */
     @RequestMapping({"/tmdb/3/**", "/tmdb/4/auth/**", "/tmdb/4/account/**"})
     public ResponseEntity<String> tmdbApi(
         HttpMethod method, HttpServletRequest request,
         @RequestBody(required = false) String body, HttpServletResponse response
     ) {
         logger.debug("tmdbApi({}, {}, {})", method, request.getRequestURI(), body);
-
         // Create path without /tmdb prefix so that is it compatible
         // with the original `tmdb` api
         String path = request
             .getRequestURI()
             .replaceAll("^/tmdb", "");
-
         return client.fetchTmdbApi(method, path, request, body);
+    }
+
+
+    /**
+     * Endpoint that proxies all tmdb image methods.
+     */
+    @GetMapping(value = "/image.tmdb/**", produces = "application/octet-stream")
+    public void tmdbImage(
+        HttpMethod method, HttpServletRequest request,
+        @RequestBody(required = false) String body, HttpServletResponse response
+    ) {
+        logger.debug("tmdbImage({}, {}, {})", method, request.getRequestURI(), body);
+        // Create path without /image.tmdb prefix so that is it compatible
+        // with the original `tmdb` image api
+        String path = request
+            .getRequestURI()
+            .replaceAll("^/image.tmdb", "");
+        client.fetchTmdbImage(method, path, request, body, response);
     }
 
     // TODO: Move to own feature
@@ -67,86 +71,10 @@ public class ProxyController {
     @RequestMapping({"/tmdb/4/list/**", "/tmdb/4/{_}"})
     @SuppressWarnings("MVCPathVariableInspection")
     public ResponseEntity<String> listActions(@RequestBody(required = false) String body,
-                                     HttpMethod method, HttpServletRequest request, HttpServletResponse response) {
+                                              HttpMethod method, HttpServletRequest request, HttpServletResponse response) {
 
         logger.debug("Custom List Action | {} | {}", method.name(), request.getRequestURI());
 
         return tmdbApi(method, request, body, response);
-    }
-
-    @GetMapping(value = "/image.tmdb/**", produces = "application/octet-stream")
-    public void image(@RequestBody(required = false) String body,
-                                              HttpMethod method, HttpServletRequest request, HttpServletResponse response)
-        throws URISyntaxException, IOException {
-
-        logger.debug("Image | {} | {}", method.name(), request.getRequestURI());
-
-        // TODO: Create tmdb client
-
-        // Based on https://stackoverflow.com/a/49429650/12347616
-        String requestUrl = request
-            .getRequestURI()
-            .replaceAll("^/image.tmdb", "");
-
-        URI uri = new URI("https", null, "image.tmdb.org", 443, null, null, null);
-        uri = UriComponentsBuilder.fromUri(uri)
-            .path(requestUrl)
-            .query(request.getQueryString())
-            .build(true).toUri();
-
-        // Set all headers besides encoding (explained later)
-        HttpHeaders headers = new HttpHeaders();
-        Enumeration<String> headerNames = request.getHeaderNames();
-        while (headerNames.hasMoreElements()) {
-            String headerName = headerNames.nextElement();
-            if (Objects.equals(headerName, "Accept-Encoding")) continue;
-            headers.set(headerName, request.getHeader(headerName));
-        }
-
-        // Fix encoding
-        // Lead to 'Undocumented Error:OK' on Swagger
-        // Debugger showed (in Chromium only) net::ERR_INCOMPLETE_CHUNKED_ENCODING 200 (OK)
-        // Fix: https://stackoverflow.com/questions/54852953/postman-shows-could-not-get-any-response-even-though-response-is-ok
-        headers.set("Accept-Encoding", "*/*");
-
-        // API key
-        // TODO: Move to config
-        headers.set("Authorization", "Bearer " + "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzY2ZjNmUxZGQyMzFiZDFmMmNhYTE5OGU3MzE3YTZhNCIsInN1YiI6IjYwZWZiOTZlYTQ0ZDA5MDAyZDQ0ZjNlNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nvvleDHS5FWTK9UbhKfeuW8L5w4hyjGHAphNtQJuYSY");
-
-        HttpEntity<String> httpEntity = new HttpEntity<>(body, headers);
-
-        // TODO Add restTemplate bean in order to mock
-        RestTemplate restTemplate = new RestTemplate();
-        try {
-            // Based on https://stackoverflow.com/a/62528659/12347616
-            restTemplate.execute(uri, method, null, clientHttpResponse -> {
-                StreamUtils.copy(clientHttpResponse.getBody(), response.getOutputStream());
-                return null;
-            });
-        } catch(HttpStatusCodeException e) {
-            populateResponse(
-                ResponseEntity.status(e.getStatusCode())
-                     .headers(e.getResponseHeaders())
-                     .body(e.getResponseBodyAsString()),
-                response
-            );
-        }
-    }
-
-    // https://stackoverflow.com/a/51655139/12347616
-    public static void populateResponse(ResponseEntity<String> responseEntity, HttpServletResponse servletResponse)
-        throws IOException {
-        for (Map.Entry<String, List<String>> header : responseEntity.getHeaders().entrySet()) {
-            String chave = header.getKey();
-            for (String valor : header.getValue()) {
-                servletResponse.addHeader(chave, valor);
-            }
-        }
-
-        servletResponse.setStatus(responseEntity.getStatusCode().value());
-        if (responseEntity.hasBody()) {
-            servletResponse.getWriter().write(responseEntity.getBody());
-        }
-
     }
 }
