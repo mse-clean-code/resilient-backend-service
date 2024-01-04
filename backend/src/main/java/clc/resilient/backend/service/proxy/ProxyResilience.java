328ccd3430669a0c64634a92c08b6860458ea45d
Kacper Urbaniec <at.kacper.urbaniec@gmail.com>
2024-01-05 00:04:01 +0100

    Added some clarifying comments about `ProxyResilience`

--- backend/src/main/java/clc/resilient/backend/service/proxy/ProxyResilience.java
+++ backend/src/main/java/clc/resilient/backend/service/proxy/ProxyResilience.java
@@ -5,6 +5,9 @@ package clc.resilient.backend.service.proxy;
  * @version 2024-01-04
  */
 public class ProxyResilience {
+    // Names must match the config in `application.properties`
+    // For example, PROXY RETRY with "proxyRetryApi" references
+    // 'resilience4j.retry.instances.proxyRetryApi' in the config!
     public static final String PROXY_RETRY = "proxyRetryApi";
     public static final String PROXY_CIRCUIT_BREAKER = "proxyCircuitBreaker";
     public static final String PROXY_RATE_LIMITER = "proxyRateLimiterApi";
