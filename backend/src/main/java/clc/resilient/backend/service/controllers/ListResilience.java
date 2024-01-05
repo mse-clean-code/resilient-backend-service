757ea82bf0065f7d8311e56433fe9d29ec373a04
se22m053 <se22m053@technikum-wien.at>
2024-01-05 14:00:59 +0100

    Adding Resilience4j Retry pattern to all listController Endpoints

---
+++ backend/src/main/java/clc/resilient/backend/service/controllers/ListResilience.java
@@ -0,0 +1,7 @@
+package clc.resilient.backend.service.controllers;
+
+public class ListResilience {
+    // Names must match the config in `application.properties`
+    public static final String LIST_RETRY = "listRetry";
+
+}
